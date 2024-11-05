55bf3f5bb1431cbcaf3abbff7381d1ee51c6bf20
TomasAlric <tomas.alricc@gmail.com>
2024-11-05 03:01:40 -0300

    feat: first day

---
+++ cell01/ex00/name.py
@@ -0,0 +1,4 @@
+first_name = "Tomas"
+last_name  = "Alric"
+
+print(f'{first_name} {last_name}')
---
+++ cell01/ex01/name.py
@@ -0,0 +1,5 @@
+first_name = "Tomas"
+last_name  = "Alric"
+whole_name = first_name + " " + last_name
+
+print(whole_name)
